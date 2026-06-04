0a985231c049c73519949e0bb5fd0ca26fd07d10
Fahad Ahmed <fahad838@gmail.com>
2026-06-04 19:28:50 +0530

    initailize struct and start program

--- playlist/main.go
+++ playlist/main.go
@@ -1,5 +1,22 @@
 package main
 
-func main(){
-	
+import "github.com/Sheikh-Fahad-Ahmed/go-review/playlist/songs"
+
+func main() {
+
+	newPlaylist := playlist{}
+
+	song1 := songs.Create("Yellow", "Coldplay")
+	song2 := songs.Create("Sweden", "C418")
+	song3 := songs.Create("Drag Path", "Twenty one Pilots")
+
+	newPlaylist.Add(song1)
+	newPlaylist.Add(song2)
+	newPlaylist.Add(song3)
+
+	newPlaylist.Play()
+	newPlaylist.Next()
+	newPlaylist.Next()
+	newPlaylist.Next()
+
 }
