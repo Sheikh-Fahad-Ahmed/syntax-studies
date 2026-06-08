a907440bc65d24c28baab130395ed8df7c8cd437
Fahad Ahmed <fahad838@gmail.com>
2026-06-08 14:59:05 +0530

    Create component: Header, Menu, Footer

--- react-review/react-practice/my-react/src/App.jsx
+++ react-review/react-practice/my-react/src/App.jsx
@@ -1,14 +1,47 @@
 function App() {
   return (
     <div>
-      <h1>Hello To Your React App</h1>;
+      <Header />
+      <Menu />
+
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <div>
+      <h1>Fast React Pizza Co.</h1>
+    </div>
+  );
+}
+
+function Menu() {
+  return (
+    <div>
+      <h2>Our Menu</h2>
+      <Pizza />
+      <Pizza />
       <Pizza />
     </div>
   );
 }
 
+function Footer() {
+  return (
+    <footer>{new Date().toLocaleTimeString()} We're currently open!</footer>
+  );
+}
+
 function Pizza() {
-  return <h2>Pizza</h2>;
+  return (
+    <div>
+      <img src="pizzas/spinaci.jpg" alt="Pizza spinaci" />
+      <h2>Pizza Spinaci</h2>
+      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
+    </div>
+  );
 }
 
 export default App;
