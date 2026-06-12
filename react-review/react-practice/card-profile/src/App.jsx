2afea597f4086c4aadc6166f54fc5a1bf02f5004
Fahad Ahmed <fahad838@gmail.com>
2026-06-12 10:28:14 +0530

    Inital commit

---
+++ react-review/react-practice/card-profile/src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return <img src="./assets/artorias.jpg" alt="artorias" />;
+}
+
+export default App
