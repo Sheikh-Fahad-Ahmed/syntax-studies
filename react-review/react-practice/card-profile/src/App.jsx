a6dd0c96f46e3a0560e860b51b6e88a6fa1959dc
Fahad Ahmed <fahad838@gmail.com>
2026-06-12 11:03:23 +0530

    Create: a card profile

--- react-review/react-practice/card-profile/src/App.jsx
+++ react-review/react-practice/card-profile/src/App.jsx
@@ -11,7 +11,41 @@ function App() {
 }
 
 function Avatar() {
-  return <img src="./assets/artorias.jpg" alt="artorias" />;
+  return <img className="avatar" src="/artorias.jpg" alt="artorias" />;
 }
 
-export default App
+function Intro() {
+  return (
+    <div>
+      <h1>Artorias, The Abysswalker</h1>
+      <p>
+        Thou shalt see further on, an Abyss was begat of the ancient beast, and
+        threatens to swallow the whole of Oolacile. Knight Artorias came to stop
+        this, but such a hero has nary a murmur of Dark. Without doubt he will
+        be swallowed by the Abyss, overcome by its utter blackness... Indeed,
+        the Abyss may be unstoppable...
+      </p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill skill="Charging Slash 🗡️" color="orange" />
+      <Skill skill=" Abyss Sludge 💥 " color="green" />
+      <Skill skill=" Spinning Slash ⚔️" color="red" />
+      <Skill skill=" Wrath of the Abyss ⚡" color="blue" />
+    </div>
+  );
+}
+
+function Skill(props) {
+  return (
+    <div className="skill" style={{ backgroundColor: props.color }}>
+      <p>{props.skill}</p>
+    </div>
+  );
+}
+
+export default App;
